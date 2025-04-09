e671d635f7e24977d9930711f55301726299aa7c
uKyez <123280865+uKyez@users.noreply.github.com>
2025-04-09 17:33:06 -0300

    Add config/database.php 1744230785644

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+// Database configuration
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'investment_saas',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+];
