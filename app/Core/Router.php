30c4839ece78066b9d0a15e2e7edbc352e3b6d81
uKyez <123280865+uKyez@users.noreply.github.com>
2025-04-09 17:33:14 -0300

    Add app/Core/Router.php 1744230793528

---
+++ app/Core/Router.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\Core;
+
+class Router
+{
+    protected $routes = [];
+    protected $currentRoute;
+
+    public function __construct()
+    {
+        $this->loadRoutes();
+    }
+
+    protected function loadRoutes()
+    {
+        $this->routes = [
+            'GET' => [],
+            'POST' => [],
+            'PUT' => [],
+            'DELETE' => []
+        ];
+    }
+
+    public function dispatch()
+    {
+        $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+        $method = $_SERVER['REQUEST_METHOD'];
+        
+        error_log("Current routes: " . print_r($this->routes, true));
+        error_log("Looking for: $method $uri");
+
+        if (isset($this->routes[$method][$uri])) {
+            $this->currentRoute = $this->routes[$method][$uri];
+            $this->callAction();
+        } else {
+            http_response_code(404);
+            echo "404 Not Found";
+        }
+    }
+
+    protected function callAction()
+    {
+        $controller = $this->currentRoute['controller'];
+        $action = $this->currentRoute['action'];
+        $controllerClass = "App\\Controllers\\{$controller}";
+
+        if (class_exists($controllerClass) && method_exists($controllerClass, $action)) {
+            $controllerInstance = new $controllerClass();
+            $controllerInstance->$action();
+        } else {
+            throw new \Exception("Controller or action not found");
+        }
+    }
+
+    public function addRoute($method, $uri, $controller, $action)
+    {
+        $this->routes[$method][$uri] = [
+            'controller' => $controller,
+            'action' => $action
+        ];
+    }
+}
