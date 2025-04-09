4e039f31621b5572ed7e1df27e02d92951510f1a
uKyez <123280865+uKyez@users.noreply.github.com>
2025-04-09 17:33:07 -0300

    Add config/paths.php 1744230786594

---
+++ config/paths.php
@@ -0,0 +1,8 @@
+<?php
+// Define application paths
+define('ROOT_PATH', dirname(__DIR__) . '/');
+define('APP_PATH', ROOT_PATH . 'app/');
+define('PUBLIC_PATH', ROOT_PATH . 'public/');
+define('VIEWS_PATH', APP_PATH . 'Views/');
+define('CONFIG_PATH', ROOT_PATH . 'config/');
+define('VENDOR_PATH', ROOT_PATH . 'vendor/');
